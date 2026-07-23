344d1f99fb9198741b57de4665be978274e28217
stayed_gone <25genius211@sunrint.hs.kr>
2026-07-23 13:22:07 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
-import streamlit.components.v1 as components
+import os
 
-st.set_page_config(
-    page_title="우리시골",
-    layout="wide"
-)
+st.set_page_config(layout="wide")
 
-with open("index.html","r",encoding="utf-8") as f:
-    html=f.read()
+st.title("🌿 우리시골")
 
-components.html(
-    html,
-    height=900,
-    scrolling=True
+html_file = os.path.abspath("index.html")
+
+st.markdown(
+    f"""
+    <iframe
+        src="file://{html_file}"
+        width="100%"
+        height="900"
+        style="border:none;">
+    </iframe>
+    """,
+    unsafe_allow_html=True
 )
