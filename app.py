ffb8d08decbedc482d6a902222e41bc26ca5c657
stayed_gone <25genius211@sunrint.hs.kr>
2026-07-23 13:12:30 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title="우리시골",
+    layout="wide"
+)
+
+with open("index.html","r",encoding="utf-8") as f:
+    html=f.read()
+
+components.html(
+    html,
+    height=900,
+    scrolling=True
+)
